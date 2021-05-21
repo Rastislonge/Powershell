57aff7e9d03b291ac1d610cc14934b54b01c39c2
Rastislonge <69406403+Rastislonge@users.noreply.github.com>
2021-05-20 22:11:09 -0300

    Add files via upload

---
+++ crackers - Github Copy/Crackers-Launcher.ps1
@@ -0,0 +1,2 @@
+powershell -WindowStyle hidden -File C:\Users\User\Desktop\crackers\test\keys_test.ps1
+exit
---
+++ crackers - Github Copy/cracker1.1.bat
@@ -0,0 +1,11 @@
+@echo off
+
+REM set browser=chrome.exe
+REM set wait_time=2
+REM start %browser% -new "youtube.com"
+
+timeout /t 5
+
+powershell.exe -noexit -executionpolicy bypass -file c:\users\User\desktop\crackers\ps1save.ps1
+
+pause
